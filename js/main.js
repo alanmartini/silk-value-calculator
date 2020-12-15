5b5809a5fa1ab546b4791cf408efa08547f45acf
alanmmartini <alanmmartini@gmail.com>
2020-12-15 20:21:35 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+$(function(){
+
+	$('.gold').on('focusout', function(){
+
+		$(".gold").not($(this)).val('');
+
+		var mainvalue = $(this).val().replace(",", ".");
+		console.log('valor adicionado ' + mainvalue);
+
+		var itemSilk = $(this).parents('.mb-2').find('.silk').val();
+		console.log('valor silk ' + itemSilk);
+		var mPorSilk = mainvalue / itemSilk;
+
+		$('b').html(mPorSilk.toFixed(2));
+
+		fillGoldSpots(mPorSilk);		
+	});
+
+	function fillGoldSpots(mPorSilk) {
+
+		$( ".gold" ).each(function( i ) {
+			if( !$(this).val() ) {			
+				var itemSilkLinha = $(this).parents('.mb-2').find('.silk').val();
+				total = mPorSilk * itemSilkLinha;
+				$(this).val(total.toFixed(2));
+			}
+		});
+	}
+});
